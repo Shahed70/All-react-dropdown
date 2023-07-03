9964db601c1d027da51a70c8b68b6385958f9e39
Shahed70 <58668503+Shahed70@users.noreply.github.com>
2023-07-04 00:59:31 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# All-react-dropdown
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-wnxoug)
---
+++ src/NavMenu.js
@@ -0,0 +1,291 @@
+
+// this all menu have dropdown 
+
+
+
+// import React, { useState } from 'react';
+// //import './NavMenu.css'; // Import the CSS file for styling
+
+// const menuItemsData = [
+//   {
+//     label: 'Home',
+//     submenus: ['Submenu 1', 'Submenu 2', 'Submenu 3'],
+//     isOpen: false,
+//   },
+//   {
+//     label: 'About',
+//     submenus: ['Submenu 1', 'Submenu 2', 'Submenu 3'],
+//     isOpen: false,
+//   },
+//   {
+//     label: 'Contact',
+//     submenus: ['Submenu 1', 'Submenu 2', 'Submenu 3'],
+//     isOpen: false,
+//   },
+// ];
+
+// const NavMenu = () => {
+//   const [menuItems, setMenuItems] = useState(menuItemsData);
+
+//   const toggleMenu = (index) => {
+//     const updatedMenuItems = menuItems.map((menuItem, i) => ({
+//       ...menuItem,
+//       isOpen: i === index ? !menuItem.isOpen : false,
+//     }));
+//     setMenuItems(updatedMenuItems);
+//   };
+
+//   return (
+//     <nav>
+//       <ul className="menu">
+//         {menuItems.map((menuItem, index) => (
+//           <li key={index}>
+//             <div className="menu-item">
+//               <a href="#">{menuItem.label}</a>
+//               <button className="toggle-btn" onClick={() => toggleMenu(index)}>
+//                 {menuItem.isOpen ? '-' : '+'}
+//               </button>
+//             </div>
+//             {menuItem.isOpen && (
+//               <ul className="dropdown">
+//                 {menuItem.submenus.map((submenu, subIndex) => (
+//                   <li key={subIndex}>{submenu}</li>
+//                 ))}
+//               </ul>
+//             )}
+//           </li>
+//         ))}
+//       </ul>
+//     </nav>
+//   );
+// };
+
+// export default NavMenu;
+
+
+// this all menu have dropdown 
+
+
+
+
+
+
+// this only two menu have dropdown 
+
+// import React, { useState } from 'react';
+// //import './NavMenu.css'; // Import the CSS file for styling
+
+// const menuItemsData = [
+//   {
+//     label: 'Home',
+//     isOpen: false,
+//   },
+//   {
+//     label: 'About',
+//     submenus: ['Submenu 1', 'Submenu 2', 'Submenu 3'],
+//     isOpen: false,
+//   },
+//   {
+//     label: 'Contact',
+//     isOpen: false,
+//   },
+
+//   {
+//     label: 'About2',
+//     submenus: ['Submenu 1', 'Submenu 2', 'Submenu 3'],
+//     isOpen: false,
+//   },
+// ];
+
+// const NavMenu = () => {
+//   const [menuItems, setMenuItems] = useState(menuItemsData);
+
+//   const toggleMenu = (index) => {
+//     const updatedMenuItems = menuItems.map((menuItem, i) => ({
+//       ...menuItem,
+//       isOpen: i === index ? !menuItem.isOpen : false,
+//     }));
+//     setMenuItems(updatedMenuItems);
+//   };
+
+//   return (
+//     <nav>
+//       <ul className="menu">
+//         {menuItems.map((menuItem, index) => (
+//           <li key={index}>
+//             <div className="menu-item">
+//               <a href="#">{menuItem.label}
+//               {menuItem.submenus && (
+//                 <button className="toggle-btn" onClick={() => toggleMenu(index)}>
+//                   {menuItem.isOpen ? '-' : '+'}
+//                 </button>
+//               )}</a>
+//             </div>
+//             {menuItem.isOpen && menuItem.submenus && (
+//               <ul className="dropdown">
+//                 {menuItem.submenus.map((submenu, subIndex) => (
+//                   <li key={subIndex}>{submenu}</li>
+//                 ))}
+//               </ul>
+//             )}
+//           </li>
+//         ))}
+//       </ul>
+//     </nav>
+//   );
+// };
+
+// export default NavMenu;
+
+
+// this only two menu have dropdown 
+
+
+
+
+// this dropdown doesnot have extra button next to it 
+
+// import React, { useState } from 'react';
+// //import './NavMenu.css'; // Import the CSS file for styling
+
+// const menuItemsData = [
+//   {
+//     label: 'Home',
+//     isOpen: false,
+//   },
+//   {
+//     label: 'About',
+//     submenus: ['Submenu 1', 'Submenu 2', 'Submenu 3'],
+//     isOpen: false,
+//   },
+//   {
+//     label: 'Contact',
+//     isOpen: false,
+//   },
+
+//   {
+//     label: 'About2',
+//     submenus: ['Submenu 1', 'Submenu 2', 'Submenu 3'],
+//     isOpen: false,
+//   },
+// ];
+
+// const NavMenu = () => {
+//   const [menuItems, setMenuItems] = useState(menuItemsData);
+
+//   const toggleMenu = (index) => {
+   
+//     console.log(index)
+
+//     const updatedMenuItems = menuItems.map((menuItem, i) => ({
+
+//       ...menuItem,
+//       isOpen: i === index ? !menuItem.isOpen : false,
+      
+//     }));
+//     setMenuItems(updatedMenuItems);
+//   };
+
+//   return (
+//     <nav>
+//       <ul className="menu">
+//         {menuItems.map((menuItem, index) => (
+//           <li key={index}>
+//             <div className="menu-item">
+//               <a href="#" onClick={() => toggleMenu(index)}>{menuItem.label} </a>
+//               {/* {menuItem.submenus && (
+//                 <button className="toggle-btn" onClick={() => toggleMenu(index)}>
+//                   {menuItem.isOpen ? '-' : '+'}
+//                 </button>
+//               )} */}
+//             </div>
+//             {menuItem.isOpen && menuItem.submenus && (
+//               <ul className="dropdown">
+//                 {menuItem.submenus.map((submenu, subIndex) => (
+//                   <li  key={subIndex}>{submenu}</li>
+//                 ))}
+//               </ul>
+//             )}
+//           </li>
+//         ))}
+//       </ul>
+//     </nav>
+//   );
+// };
+
+// export default NavMenu;
+
+// this dropdown doesnot have extra button next to it 
+
+
+
+
+
+// This submenu disappear When clik on submenu too
+
+import React, { useState } from 'react';
+//import './NavMenu.css'; // Import the CSS file for styling
+
+const menuItemsData = [
+  {
+    label: 'Home',
+    isOpen: false,
+  },
+  {
+    label: 'About',
+    submenus: ['Submenu 1', 'Submenu 2', 'Submenu 3'],
+    isOpen: false,
+  },
+  {
+    label: 'Contact',
+    isOpen: false,
+  },
+
+  {
+    label: 'About2',
+    submenus: ['Submenu 1', 'Submenu 2', 'Submenu 3'],
+    isOpen: false,
+  },
+];
+
+const NavMenu = () => {
+  const [menuItems, setMenuItems] = useState(menuItemsData);
+
+  const toggleMenu = (index) => {
+    const updatedMenuItems = menuItems.map((menuItem, i) => ({
+      ...menuItem,
+      isOpen: i === index ? !menuItem.isOpen : false,
+    }));
+    setMenuItems(updatedMenuItems);
+  };
+
+  return (
+    <nav>
+      <ul className="menu">
+        {menuItems.map((menuItem, index) => (
+          <li key={index}>
+            <div className="menu-item">
+              <a href="#" onClick={() => toggleMenu(index)}>{menuItem.label} </a>
+              {/* {menuItem.submenus && (
+                <button className="toggle-btn" onClick={() => toggleMenu(index)}>
+                  {menuItem.isOpen ? '-' : '+'}
+                </button>
+              )} */}
+            </div>
+            {menuItem.isOpen && menuItem.submenus && (
+              <ul className="dropdown">
+                {menuItem.submenus.map((submenu, subIndex) => (
+                  <li onClick={() => toggleMenu(index)} key={subIndex}>{submenu}</li>
+                ))}
+              </ul>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavMenu;
+
+// This submenu disappear When clik on submenu too
